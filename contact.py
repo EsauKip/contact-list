ba88e5c97fe1326caa81b45b4199b86871268395
EsauKip <esau.kiprono@student.moringaschool.com>
2022-02-15 12:34:28 +0300

    create a class that generates instance of contact

---
+++ contact.py
@@ -0,0 +1,8 @@
+class Contact:
+    """class that generates instances of a contact
+    """
+    def __init__(self,first_name,last_name,number,email):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.phone_number = number
+        self.email = email
