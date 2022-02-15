4fee3a12afa71f9e9bac207716f129139d9295df
EsauKip <esau.kiprono@student.moringaschool.com>
2022-02-15 13:21:00 +0300

    create a test file

--- contact.py
+++ contact.py
@@ -1,6 +1,7 @@
 class Contact:
     """class that generates instances of a contact
     """
+    contact_list=[]
     def __init__(self,first_name,last_name,number,email):
         self.first_name = first_name
         self.last_name = last_name
